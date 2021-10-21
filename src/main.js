077732655e9c3a50bf4afcee67d3fafc57742656
vithorluc <vithorvarela.academico@gmail.com>
2021-10-21 08:48:11 -0300

    feat: coaches vuex

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
+
+const app = createApp(App)
+
+app.use(router);
+app.use(store);
+
+app.mount('#app');
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in coaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('coaches', ['coaches', 'hasCoaches'])
+    // filteredCoaches() {
+    //   return this.$store.getters['coaches/coaches'];
+    // },
+    // hasCoaches() {
+    //   return this.$store.getters['coaches/hasCoaches'];
+    // }
+  }
+};
+</script>
