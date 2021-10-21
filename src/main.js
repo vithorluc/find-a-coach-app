488d9b0636be9a25aac634d8490f2d6da1ce85ae
vithorluc <vithorvarela.academico@gmail.com>
2021-10-21 10:11:31 -0300

    feat: base ui components

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
 
-const app = createApp(App)
+/*=======================GLOBAL UI COMPONENTS=======================*/
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+/*==================================================================*/
+
+const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
